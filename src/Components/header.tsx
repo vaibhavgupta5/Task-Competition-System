c918ee846cb910ee7bf4a5aac3688b47c348747b
vaibhavgupta5 <vaibhavgupta.v890@gmail.com>
2024-09-06 03:04:47 +0530

    frontend first commit

---
+++ src/Components/header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='w-full h-[10vh] bg-pink-500'>header</div>
+  )
+}
+
+export default Header
---
+++ src/Components/scoreboard.tsx
@@ -0,0 +1,10 @@
+'use client'
+import React from 'react'
+
+function Scoreboard() {
+  return (
+    <div>Scoreboard</div>
+  )
+}
+
+export default Scoreboard
