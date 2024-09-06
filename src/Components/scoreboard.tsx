2ada9e1ff200aa5bd0a73b6927f100af8613e254
vaibhavgupta5 <vaibhavgupta.v890@gmail.com>
2024-09-07 02:29:10 +0530

    updated scoreboard

--- src/Components/scoreboard.tsx
+++ src/Components/scoreboard.tsx
@@ -1,13 +1,53 @@
 'use client'
 import axios from 'axios'
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
  function Scoreboard() {
 
+
+interface ScoreboardData {
+    name: string;
+    score: number;
+}
+
+  const [scoreboard, setscoreboard] = useState<ScoreboardData[]>([]);
+  
     const getAllUsers = async () =>{
         try {
             const result = await axios.get("/api/getUsers")
             console.log(result.data.users)
+
+            let updatedScoreboard: any[] = [];
+
+            let score = 0;
+
+            result.data.users.map((user: any) =>{
+
+              user.daily.map((daily: any) =>{
+                score += daily.total
+              })
+
+              user.weekly.map((weekly: any) =>{
+                score += weekly.total
+              })
+
+              user.monthly.map((monthly: any) =>{
+                score += monthly.total
+              })
+
+             const dataScore  = {
+              name: user.name,
+              score: score
+             } as any
+
+             updatedScoreboard.push(dataScore);
+
+
+            })
+
+            setscoreboard(updatedScoreboard); // Set state once the loop is done
+
+
         } catch (error) {
             console.log("error", error)
         }
