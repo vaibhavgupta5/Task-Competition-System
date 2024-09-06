386869ce693a037b3d0f2e3d2ba036312e53bcae
vaibhavgupta5 <vaibhavgupta.v890@gmail.com>
2024-09-07 03:08:11 +0530

    updated dashboard

--- src/Components/scoreboard.tsx
+++ src/Components/scoreboard.tsx
@@ -1,66 +1,73 @@
-'use client'
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
+"use client";
+import axios from "axios";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
 
- function Scoreboard() {
+function Scoreboard() {
+ 
 
+  const [scoreboard, setscoreboard] = useState<any[]>([]);
 
-interface ScoreboardData {
-    name: string;
-    score: number;
-}
+  const getAllUsers = async () => {
+    try {
+      const result = await axios.get("/api/getUsers");
+      // console.log(result.data.users);
 
-  const [scoreboard, setscoreboard] = useState<ScoreboardData[]>([]);
-  
-    const getAllUsers = async () =>{
-        try {
-            const result = await axios.get("/api/getUsers")
-            console.log(result.data.users)
-
-            let updatedScoreboard: any[] = [];
-
-            let score = 0;
-
-            result.data.users.map((user: any) =>{
-
-              user.daily.map((daily: any) =>{
-                score += daily.total
-              })
-
-              user.weekly.map((weekly: any) =>{
-                score += weekly.total
-              })
-
-              user.monthly.map((monthly: any) =>{
-                score += monthly.total
-              })
-
-             const dataScore  = {
-              name: user.name,
-              score: score
-             } as any
-
-             updatedScoreboard.push(dataScore);
+      let updatedScoreboard: any[] = [];
 
 
-            })
+      result.data.users.map((user: any) => {
+        let score = 0;
 
-            setscoreboard(updatedScoreboard); // Set state once the loop is done
+        user.daily.map((daily: any) => {
+          score += Number(daily.total);
+        });
 
+        user.weekly.map((weekly: any) => {
+          score += Number(weekly.total);
+        });
 
-        } catch (error) {
-            console.log("error", error)
-        }
+        user.monthly.map((monthly: any) => {
+          score += Number(monthly.total);
+        });
+
+        const dataScore = {
+          name: user.name,
+          image: user.image,
+          score: score,
+        } as any;
+
+        // console.log(dataScore);
+
+        updatedScoreboard.push(dataScore);
+        // console.log(updatedScoreboard)
+      });
+      // console.log(updatedScoreboard)
+      setscoreboard(updatedScoreboard); // Set state once the loop is done
+
+      // console.log(scoreboard);
+    } catch (error) {
+      console.log("error", error);
     }
+  };
 
-    useEffect(()=>{
-        getAllUsers()
-    },[])
+  useEffect(() => {
+    getAllUsers();
+  }, []);
 
 
-  return (
-    <div>Scoreboard</div>
-  )
+
+  return <div>
+    <ul>
+      {scoreboard.map((user: any, index: number) => (
+        <li className="flex p-4 items-center justify-between bg-[#F5F5F8] mb-2 rounded-md" key={index}>
+          <Image className="rounded-full" src="/image.png" alt={user.name} width={50} height={50} />
+          <h2>{user.name}</h2>
+          <p>{user.score}</p>
+        </li>
+      ))}
+    </ul>
+  </div>;
 }
 
-export default Scoreboard
+export default Scoreboard;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,17 +4,18 @@ import Scoreboard from "@/Components/scoreboard";
 
 export default function Home() {
   return (
-    <div className="bg-black h-screen w-full flex flex-col items-center">
+    <div className="bg-gradient-to-r from-[#F5F5F8] to-[#D4D4DA] h-screen w-full flex flex-col items-center">
     <Header/>
       <div className="flex flex-row w-[90%] justify-center items-center ">
-        <div className="flex flex-col w-[40%] justify-center items-center text-black">
+        <div className="flex flex-col w-[30%] justify-center items-center text-black">
         <div className="bg-white h-[15vh] rounded-md flex   w-full m-2 p-4">
           <div className="flex flex-col items-start justify-center ">
             <h1 className="text-xl  ">Welcome,</h1>
             <h1 className="text-3xl font-bold  ">Harshit Chaturvedi</h1>
             </div>
         </div>
-        <div className="bg-white rounded-md h-[70vh] w-full m-2">
+        <div className="bg-white p-4 rounded-md h-[70vh] w-full m-2">
+          <h1 className="font-medium text-xl pb-4">Scoreboard</h1>
           <Scoreboard/>
         </div>
         </div>
